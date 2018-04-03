78d9eafb411f08d0518f403bdca4449feaa58b65
Axe1701 <37373371+Axe1701@users.noreply.github.com>
2018-04-03 09:31:34 -0500

    Update Trabajo1.6.cpp

--- Trabajo 1-Ccomp I/Trabajo1.6.cpp
+++ Trabajo 1-Ccomp I/Trabajo1.6.cpp
@@ -3,22 +3,23 @@
 using namespace std;
 
 int main(){
-	int n,a=1,z=1,cant_mult=0;
+	int n,a=1,z=1,cant_mult=0,x=0;
 	
 	cout<<"Dame un numero: ";cin>>n;
 	
-	while(a<n){
+	while(x!=n){
 		cant_mult=0;
 		z=1;
 		while(z<=a){
-         if(a%z==0)
-            cant_mult+=1;
-        z+=1;
-    	}
+         		if(a%z==0){
+            			cant_mult+=1;
+        			z+=1;}
+    		}
     
-	    if(cant_mult==2){
-	    	cout<<a<<endl;
-	    }  
+	    	if(cant_mult==2){
+	    		cout<<a<<endl;
+			x+=1;
+	   	}  
 		a+=1;
 		
 	}
