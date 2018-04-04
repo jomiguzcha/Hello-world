e147a62029a24154d593c8be2ea28abc350a2c78
Axe1701 <37373371+Axe1701@users.noreply.github.com>
2018-04-04 09:16:57 -0500

    Update Trabajo1.4.cpp

--- Trabajo 1-Ccomp I/Trabajo1.4.cpp
+++ Trabajo 1-Ccomp I/Trabajo1.4.cpp
@@ -4,38 +4,17 @@ using namespace std;
 
 int main(){
 	int a,b;
-	
-	cout<<"Dame dos numeros: ";cin>>a>>b;
-	
-	if(a%2==0){
-		cout<<a<<" es multiplo de 2"<<endl;}
-	else{
-		cout<<a<<" no es multiplo de 2"<<endl;
-	}
-	
-	if(b%2==0){
-		cout<<b<<" es multiplo de 2"<<endl;
-	}
-	else{
-		cout<<b<<" no es multiplo de 2"<<endl;
-	}
 
-	if(a%b==0){
-		cout<<a<<" es multiplo de "<<b<<endl;
-	}
-	else{
-		cout<<a<<" no es multiplo de "<<b<<endl;
-	}
-	
-	
-	if(a*a==b){
-		cout<<a<<" elevado al cuadrado da "<<b<<endl;
-	}
-	else{
-		cout<<a<<" elevado al cuadrado NO da "<<b<<endl;
-	}
-	
-	
-	
+	cout<<"Dame dos numeros: ";cin>>a>>b;
+
+	!(a%2)?cout<<a<<" es multiplo de 2"<<endl:cout<<a<<" no es multiplo de 2"<<endl;
+    !(b%2)?cout<<b<<" es multiplo de 2"<<endl:cout<<b<<" no es multiplo de 2"<<endl;
+    !(a%b)?cout<<a<<" es multiplo de "<<b<<endl:cout<<a<<" no es multiplo de "<<b<<endl;
+    b==a*a?cout<<a<<" elevado al cuadrado da "<<b<<endl:cout<<a<<" elevado al cuadrado NO da "<<b<<endl;
+
+
+
 	return 0;
 }
+
+
