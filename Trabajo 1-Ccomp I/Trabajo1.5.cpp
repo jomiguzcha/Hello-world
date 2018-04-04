5cca0a31223e1090387d001abf0b6a8ac501de47
Axe1701 <37373371+Axe1701@users.noreply.github.com>
2018-04-04 09:25:07 -0500

    Update Trabajo1.5.cpp

--- Trabajo 1-Ccomp I/Trabajo1.5.cpp
+++ Trabajo 1-Ccomp I/Trabajo1.5.cpp
@@ -5,21 +5,14 @@ using namespace std;
 int main(){
     int x,cant_mult=0,z=1;
     cout<<"Dame un numero: ";cin>>x;
-    
-    
+
     while(z<=x){
-         if(x%z==0)
-            cant_mult+=1;
+        !(x%z)?cant_mult+=1:cant_mult=cant_mult;
         z+=1;
     }
-    
-    if(cant_mult>2){
-    	cout<<"El numero no es primo"<<endl;
-    }  
-    else{
-    	cout<<"El numero es primo"<<endl;
-    }
-        
-    
+
+    cant_mult>2?cout<<"El numero no es primo"<<endl:cout<<"El numero es primo"<<endl;
+
 	return 0;
 }
+
