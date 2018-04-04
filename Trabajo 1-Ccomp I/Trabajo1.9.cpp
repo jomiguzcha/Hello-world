bd81fdc834ca0ac79f7515fca3b5623cfa68bdc7
Axe1701 <37373371+Axe1701@users.noreply.github.com>
2018-04-04 09:55:17 -0500

    Update Trabajo1.9.cpp

--- Trabajo 1-Ccomp I/Trabajo1.9.cpp
+++ Trabajo 1-Ccomp I/Trabajo1.9.cpp
@@ -6,16 +6,9 @@ int main(){
 	int anio ;
     cout<<"Año a comprobar: ";cin>> anio ;
 
-    if (anio % 4 == 0 ){
-            if(anio%400==0 or anio%100!=0){
-                cout<<"El año es bisiesto"<<endl;}
-            else{
-                cout<<"El año no es bisiesto"<<endl;}
-            }
-
-    else{
-    	cout<<"El año no es bisiesto"<<endl;
-    }
+    !(anio % 4)? !(anio%400) or (anio%100!=0)?cout<<"El año es bisiesto"<<endl:cout<<"El año no es bisiesto"<<endl:cout<<"El año no es bisiesto"<<endl;
 
 	return 0;
 	}
+
+
