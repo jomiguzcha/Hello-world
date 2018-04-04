2fc622f56a82ed23f42d3d44a0e5a42c0a2f56c9
Axe1701 <37373371+Axe1701@users.noreply.github.com>
2018-04-04 09:02:09 -0500

    Update Trabajo1.3.cpp

--- Trabajo 1-Ccomp I/Trabajo1.3.cpp
+++ Trabajo 1-Ccomp I/Trabajo1.3.cpp
@@ -3,28 +3,19 @@
 using namespace std;
 
 int main(){
-	int a,b,c,n,prom=0,may=0,men=0;
+	int a,b,c,prom=0,may=0,men=0;
 
-    cout<<"Cantidad de numeros: ";cin>>n;
-    cout<<"Dame los numeros: ";cin>>a>>b>>c;
+    cout<<"Dame tres numeros: ";cin>>a>>b>>c;
 
-    prom=(a+b+c)/n;
+    prom=(a+b+c)/3;
 
-    if(a>b){
-        may=a;}
-    else{
-        may=b;}
-    if(c>may){
-        may=c;}
-  
-    if(a<b){
-        men=a;}
-    else{
-        men=b;}
-    if(c<men){
-        men=c;}
-    
+    may=(a>b)?a:b;
+    may=(c>may)?c:may;
+
+    men=(a<b)?a:b;
+    men=(c<men)?c:men;
+
     cout<<"Promedio: "<<prom<<"\t,el mayor: "<<may<<"\t,el menor: "<<men;
-	
+
 	return 0;
 }
