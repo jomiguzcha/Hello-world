0687320501e9869694e512027af428b9cd0f758a
Axe1701 <37373371+Axe1701@users.noreply.github.com>
2018-04-10 09:34:23 -0500

    Update Trabajo1.3.cpp

--- Trabajo 1-Ccomp I/Trabajo1.3.cpp
+++ Trabajo 1-Ccomp I/Trabajo1.3.cpp
@@ -2,20 +2,18 @@
 
 using namespace std;
 
+void ecuacion(int a,int b,int c,int &prom,int &may,int &men){
+    prom=(a+b+c)/3;
+    may=(a>b)?a:b; may=(c>may)?c:may;
+    men=(a<b)?a:b; men=(c<men)?c:men;
+}
+
 int main(){
 	int a,b,c,prom=0,may=0,men=0;
 
     cout<<"Dame tres numeros: ";cin>>a>>b>>c;
+    ecuacion(a,b,c,prom,may,men);
 
-    prom=(a+b+c)/3;
-
-    may=(a>b)?a:b;
-    may=(c>may)?c:may;
-
-    men=(a<b)?a:b;
-    men=(c<men)?c:men;
-
-    cout<<"Promedio: "<<prom<<"\t,el mayor: "<<may<<"\t,el menor: "<<men;
-
+    cout<<"Promedio: "<<prom<<",el mayor: "<<may<<",el menor: "<<men;
 	return 0;
 }
