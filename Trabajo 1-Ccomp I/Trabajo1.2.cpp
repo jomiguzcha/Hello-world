f0df7fd612dadffc5ad8efbfdbdf7440027b0b37
Axe1701 <37373371+Axe1701@users.noreply.github.com>
2018-04-10 08:57:44 -0500

    Update Trabajo1.2.cpp

--- Trabajo 1-Ccomp I/Trabajo1.2.cpp
+++ Trabajo 1-Ccomp I/Trabajo1.2.cpp
@@ -2,20 +2,23 @@
 
 using namespace std;
 
-int main(){
-	int n,x=1;
-	
-	cout<<"Dame un numero: ";cin>>n;
-	
-	while(x<=n){
+int sucesion(int n,int x=1)
+{
+   while(x<=n){
 		cout<<x;
 		if(x!=n){
 			cout<<",";
 		}
 		x+=1;
-	}	
-	
-	
-	
+	}
+
+}
+
+int main(){
+	int n;
+
+	cout<<"Dame un numero: ";cin>>n;
+    sucesion(n);
+
 	return 0;
 }
