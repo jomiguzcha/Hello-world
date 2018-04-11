c6892d93d6b31f769024c500ca528c008c2c0872
Axe1701 <37373371+Axe1701@users.noreply.github.com>
2018-04-11 09:48:17 -0500

    Update Trabajo1.7.cpp

--- Trabajo 1-Ccomp I/Trabajo1.7.cpp
+++ Trabajo 1-Ccomp I/Trabajo1.7.cpp
@@ -1,20 +1,20 @@
 #import<iostream>
 
 using namespace std;
+void CincoDigitos(int &a,int &b,int &c,int &d,int &e,int n){
+    a=n%10;n/=10;
+    b=n%10;n/=10;
+	c=n%10;n/=10;
+    d=n%10;n/=10;
+    e=n%10;
+}
 
 int main(){
-	
-    int x,a=0,b=0,c=0,d=0,e=0;
+    int n,a=0,b=0,c=0,d=0,e=0;
 
-    cout<<"Numero de cinco digitos: ";cin>>x;
-
-    a=x%10;x/=10;
-    b=x%10;x/=10;
-	c=x%10;x/=10;
-    d=x%10;x/=10;
-    e=x%10;x/=10;
-    
+    cout<<"Numero de cinco digitos: ";cin>>n;
+    CincoDigitos(a,b,c,d,e,n);
     cout<<e<<"\t"<<d<<"\t"<<c<<"\t"<<b<<"\t"<<a;
-	
+
 	return 0;
 }
