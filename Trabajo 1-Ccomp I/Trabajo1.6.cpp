daa8409d87faffe166914219a09899b636f38d2e
Axe1701 <37373371+Axe1701@users.noreply.github.com>
2018-04-11 09:32:56 -0500

    Update Trabajo1.6.cpp

--- Trabajo 1-Ccomp I/Trabajo1.6.cpp
+++ Trabajo 1-Ccomp I/Trabajo1.6.cpp
@@ -1,27 +1,25 @@
 #import<iostream>
 
 using namespace std;
+int esPrimo(int &n,int &i,int &cant_mult){
+    while(i<=n){
+        !(n%i)?cant_mult+=1:cant_mult=cant_mult;
+        i++;
+    }return cant_mult;
+}
+
 
 int main(){
-	int n,a=1,z=1,x=0,cant_mult=0;
+    int n,a=1,z=1,x=0,cant_mult=0;
 
 	cout<<"Dame un numero: ";cin>>n;
-
-	while(x!=n){
+    while(x!=n){
 		cant_mult=0;
 		z=1;
-		while(z<=a){
-            !(a%z)?cant_mult+=1:cant_mult=cant_mult;
-            z+=1;
-        }
-
-        (cant_mult==2)?cout<<a<<endl && (x+=1):x=x;
-
-		a+=1;
-
+		esPrimo(a,z,cant_mult);
+		(cant_mult==2)?cout<<a<<endl && (x++):x=x;
+		a++;
 	}
 
 	return 0;
 }
-
-
