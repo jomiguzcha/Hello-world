9ceaccc71fca7dc2f567884c1859178359561352
Axe1701 <37373371+Axe1701@users.noreply.github.com>
2018-04-11 09:57:10 -0500

    Update Trabajo1.8.cpp

--- Trabajo 1-Ccomp I/Trabajo1.8.cpp
+++ Trabajo 1-Ccomp I/Trabajo1.8.cpp
@@ -1,21 +1,20 @@
 #import<iostream>
 
 using namespace std;
+bool palindromo(int a,int b,int c,int d, int e,int n){
+    a=n%10;n/=10;
+    b=n%10;n/=10;
+    c=n%10;n/=10;
+    d=n%10;n/=10;
+    e=n%10;
+    return (a==e) && (b==d)?true:false;
+}
 
 int main(){
 	int x,a=0,b=0,c=0,d=0,e=0;
 
     cout<<"Numero: ";cin>>x;
-
-    a=x%10;x/=10;
-    b=x%10;x/=10;
-    c=x%10;x/=10;
-    d=x%10;x/=10;
-    e=x%10;x/=10;
-
-    (a==e) && (b==d)?cout<<"El numero es palindromo":cout<<"El numero NO es palindromo";
+    palindromo(a,b,c,d,e,x)?cout<<"El numero es palindromo":cout<<"El numero NO es palindromo";
 
 	return 0;
 	}
-
-
