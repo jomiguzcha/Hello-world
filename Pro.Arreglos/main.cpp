de993c1317a9c29e84221f48423bddab55b4f529
Axe1701 <37373371+Axe1701@users.noreply.github.com>
2018-05-22 09:58:12 -0500

    Update main.cpp

--- Pro.Arreglos/main.cpp
+++ Pro.Arreglos/main.cpp
@@ -1,16 +1,25 @@
 #include <iostream>
-#include "arregloenteros.h"
+#include "Arreglo.h"
 #include <stdlib.h>
+
 
 using namespace std;
 
 
 int main() {
-	Arreglo a1([1,2,3],3);
-	
+	Arreglo a1(3);
+    a1.arr[0]=0;
+    a1.arr[1]=5;
+    a1.arr[2]=9;
+
 	a1.anadir(4);
-	a1.eliminar(3);
+	a1.imprimir();
+
+	a1.eliminar(5);
+	a1.imprimir();
+
 	a1.vaciar();
-	
+	a1.imprimir();
+
 	return 0;
 }
