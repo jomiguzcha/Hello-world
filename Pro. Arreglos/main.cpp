e5fa06f90091fde0963cf0fca2053ae6ad7fbdcf
Axe1701 <37373371+Axe1701@users.noreply.github.com>
2018-05-23 09:55:14 -0500

    Update main.cpp

--- Pro. Arreglos/main.cpp
+++ Pro. Arreglos/main.cpp
@@ -19,8 +19,16 @@ int main() {
 	a1.insertar(1,7);
 	a1.mostrar();
 
-	a1.vaciar();
-	a1.mostrar();
+	//a1.vaciar();
+	//a1.mostrar();
+
+    cout<<"El mayor es : "<<a1.mayor()<<endl;
+    cout<<"El menor es : "<<a1.menor()<<endl;
+    cout<<"El promedio de elementos es : "<<a1.promedio()<<endl;
+    cout<<"El mayor primo es : "<<a1.mayor_prim()<<endl;
+    cout<<"El menor primo es : "<<a1.menor_prim()<<endl;
+
+    cout<<"La suma de elementos es : "<<a1.sumar()<<endl;
 
 
 	return 0;
