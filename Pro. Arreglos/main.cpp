23eb687678db92beeb133d5c78eb01674d91e3d9
Axe1701 <37373371+Axe1701@users.noreply.github.com>
2018-05-23 09:00:33 -0500

    Update main.cpp

--- Pro. Arreglos/main.cpp
+++ Pro. Arreglos/main.cpp
@@ -8,23 +8,20 @@ using namespace std;
 
 int main() {
 	Arreglo a1(4);
-	a1.arr[0]=3;
-	a1.arr[1]=5;
-	a1.arr[2]=1;
-	a1.arr[3]=9;
 
+    a1.iniciar();
 	a1.anadir(10);
 	a1.mostrar();
-	
+
 	a1.eliminar(2);
 	a1.mostrar();
-	
+
 	a1.insertar(1,7);
 	a1.mostrar();
-	
+
 	a1.vaciar();
 	a1.mostrar();
-	
+
 
 	return 0;
 }
